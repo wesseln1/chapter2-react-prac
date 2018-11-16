fffac362e4105c5930572335fc562bb649a25b18
wesseln1 <nwessel429@gmail.com>
2018-11-16 14:43:43 -0600

    first commit

---
+++ src/Components/Kennel.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react"
+import EmployeeList from "./employee/EmployeeList"
+import LocationList from "./Locations/"
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom"
+import React from 'react'
+import Kennel from "./components/kennel"
+
+ReactDOM.render(<Kennel />, document.querySelector("#root"));
